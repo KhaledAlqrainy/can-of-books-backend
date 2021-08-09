d3818329a635a03d6ab9e8d4c3a689232ffe641a
KhaledAlqrainy <khaledalqrainy@gmail.com>
2021-08-09 16:13:48 +0300

    finish with the backend

---
+++ modules/GetBooks.js
@@ -0,0 +1,27 @@
+const BookSchema = require('./BookSchema')
+
+function getBooks(req,res) {
+
+    const myEmail = req.query.name;
+    BookSchema.find({email:myEmail}, function (err,bookData){
+      if (bookData.length== 0 || err) {
+        res.status(404).send('cant find any books');
+      }
+      else{
+        let bookArr=bookData.map(i => {
+          return new BooksMan (i)
+        })
+        res.status(200).send(bookArr);
+      }
+    })
+}
+
+class BooksMan {
+  constructor(i){
+    this.title= i.title;
+    this.description= i.description;
+    this.status= i.status;
+  }
+}
+
+module.exports=getBooks
